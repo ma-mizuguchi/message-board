03c61a92def13639966e056136c6d9680dda36c3
masahito.mizuguchi <msht9999@gmail.com>
2019-06-03 11:50:07 +0000

    add column

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -31,10 +31,12 @@ class MessagesController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'title' => 'required|max:191',   // 追加
             'content' => 'required|max:191',
         ]);
 
         $message = new Message;
+        $message->title = $request->title;    // 追加
         $message->content = $request->content;
         $message->save();
 
@@ -65,10 +67,12 @@ class MessagesController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'title' => 'required|max:191',   // 追加
             'content' => 'required|max:191',
         ]);
 
         $message = Message::find($id);
+        $message->title = $request->title;    // 追加
         $message->content = $request->content;
         $message->save();
 
